fb712123e6fa9d3f1da74a27a0dc5eeaea7a971a
Mascal <carlomascellani@tiscali.it>
2017-04-04 14:14:24 +0200

    Update spider.py

--- script/spider.py
+++ script/spider.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-import thread, time
+import thread, time, cv2
 import numpy as np
 from gpiozero import Motor, PWMLED
 
@@ -50,6 +50,15 @@ def Rotate(angle,spd):
         time.sleep(ANG_SPD(angle/abs(spd)))
     head.stop()    
     Facing=(Facing-(angle*(np.sign(spd))))%360
+    
+# Returns the angles of the brightest spot in the panoramic frame (x = horizontal, y = vertical)
+def FindBrightestSpot(img):
+    gray = cv2.cvtColor(dewarp.panorama, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (11, 11), 0)
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(gray)
+    X = ((dewarp.Wd-maxLoc[0])*360)/dewarp.Wd # Offset to be added to fix start position of the panorama
+    Y = ((dewarp.Hd-maxLoc[0])*30)/dewarp.Hd # Should be fixed after computing the right vertical FOV of the panorama
+    return X,Y
 
 # Main program
 if __name__ == '__main__':
