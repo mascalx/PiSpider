5563ad9e56de827afff124ca5ca949b7f2492a45
Mascal <carlomascellani@tiscali.it>
2017-04-12 14:57:03 +0200

    Create update.sh

---
+++ script/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mascalx/PiSpider/master/script/spider.py
+wget https://raw.githubusercontent.com/mascalx/PiSpider/master/script/TFT.py
+wget https://raw.githubusercontent.com/mascalx/PiSpider/master/script/dewarp.py
+wget https://raw.githubusercontent.com/mascalx/PiSpider/master/script/eyelib.py
