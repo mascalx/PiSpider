392f060d9bba2209ba285d78b5d1841c267f6fb6
Mascal <carlomascellani@tiscali.it>
2017-04-29 17:44:03 +0200

    Update spider.py

--- script/spider.py
+++ script/spider.py
@@ -46,10 +46,10 @@ import eyelib
 import dewarp
 
 # Constants
-M_FWD = 5 # GPIO5 pin for forward movement
-M_BWD = 6 # GPIO6 pin for backward movement
-M_CKW = 20 # GPIO20 pin for clockwise rotation
-M_CCW = 21 # GPIO21 pin for counterclockwise rotation
+M_FWD = 20 # GPIO5 pin for forward movement
+M_BWD = 21 # GPIO6 pin for backward movement
+M_CKW = 5 # GPIO20 pin for clockwise rotation
+M_CCW = 6 # GPIO21 pin for counterclockwise rotation
 BLIGHT = 12 # GPIO12 pin for TFT backlight control
 LBUMP = 19 # GPIO19 pin for left bumper sensor
 RBUMP = 26 # GPIO26 pin for right bumper sensor
